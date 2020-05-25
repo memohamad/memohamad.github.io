0a3d37a0c4d2779ad5c0184a9a544a1e3dc3ee6e
Me Adam <me.mohamad.eko.prasetyo@gmail.com>
2020-05-25 10:47:16 +0700

    update img

--- js/flash.js
+++ js/flash.js
@@ -1,5 +1,18 @@
    
-     $(window).on('load',function(e){
+   /*  $(window).on('load',function(e){
         $("#div-1").load("div-1.html");
-        //$("#div-2").load("div-2.html");
+        $("#div-2").load("div-2.html");
+        $("#div-3").load("div-3.html");
+     });*/
+
+    $('#div-click-1').on('click',function(e){
+     	$("#div-1-1").load("div-1a.html")
+     });
+
+    $('#div-click-2').on('click',function(e){
+     	$("#div-1-1").load("div-1b.html")
+     });
+
+    $('#div-click-3').on('click',function(e){
+     	$("#div-1-1").load("div-1c.html")
      });
