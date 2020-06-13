faff9313ff2a186b7fe62f84431303dd96444a2e
meprasetyo <me.serj.adam@gmail.com>
2020-06-13 22:37:12 +0700

    up best

---
+++ en/flash.js
@@ -0,0 +1,17 @@
+   
+     $(window).on('load',function(e){
+        $("#price").load("price.html");
+   
+     });
+
+    $('#div-click-1').on('click',function(e){
+     	$("#div-1-1").load("div-1a.html");
+     });
+
+    $('#div-click-2').on('click',function(e){
+     	$("#div-1-1").load("div-1b.html");
+     });
+
+    $('#div-click-3').on('click',function(e){
+     	$("#div-1-1").load("div-1c.html");
+     });
--- js/flash-en.js
+++ js/flash-en.js
@@ -5,15 +5,15 @@
      });
 
     $('#div-click-1').on('click',function(e){
-     	$("#div-1-1").load("../en/en/div-1a.html");
+     	$("#div-1-1").load("../en/div-1a.html");
      });
 
     $('#div-click-2').on('click',function(e){
-     	$("#div-1-1").load("../en/en/div-1b.html");
+     	$("#div-1-1").load("../en/div-1b.html");
      });
 
     $('#div-click-3').on('click',function(e){
-     	$("#div-1-1").load("../en/en/div-1c.html");
+     	$("#div-1-1").load("../en/div-1c.html");
      });
 
    /* $('#en').on('click',function(e){
